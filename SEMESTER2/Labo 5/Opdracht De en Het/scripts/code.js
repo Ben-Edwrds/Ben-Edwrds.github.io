458ec40893b02d65ed3c0f1b7e29b35ae191737d
Ben-Edwrds <ben.edwards@student.vives.be>
2025-03-17 22:46:54 +0100

    a

--- SEMESTER2/Labo 5/Opdracht De en Het/scripts/code.js
+++ SEMESTER2/Labo 5/Opdracht De en Het/scripts/code.js
@@ -1,23 +1,28 @@
 const vervangAlles = (bronTekst, oud, nieuw) => {
-    let result=""
-    let idx=bronTekst.indexOf(bronTekst);
-    while(idx!=1) {
-        bronTekst.indexOf(idx + oud.length, oud)
-        result = bronTekst.substring(idx, )
-        let voor = bronTekst.slice(0,idx);
-        let na = bronTekst.slice(idx+oud.length, brontekst.length);
-        result = voor + nieuw + na;
-    }
-    return result;
+    let result = bronTekst;
+    let idx = result.toLowerCase().indexOf(oud.toLowerCase());
 
-    return result
-}
+    while (idx !== -1) {
+        let voor = result.slice(0, idx);
+        let na = result.slice(idx + oud.length);
+        result = voor + nieuw + na;
+        idx = result.toLowerCase().indexOf(oud.toLowerCase(), idx + nieuw.length);
+    }
+
+    return result;
+};
 
 const setup = () => {
-    let inputTekst = "blablabla De bradmqjdmfjmqlkdj De De mlkdqjf";
-    let outputTekst = vervangAlles(inputTekst, "de", "het");
-    console.log(outputTekst);
-}
+    let btnSend = document.getElementById("btnSend");
 
+    btnSend.addEventListener("click", () => {
+        let inputField = document.getElementById("invoer");
+        let outputField = document.getElementById("output");
+        let inputTekst = inputField.value;
+
+        let outputTekst = vervangAlles(inputTekst, "de", "het");
+        outputField.textContent = outputTekst;
+    });
+};
 
 window.addEventListener("load", setup);
